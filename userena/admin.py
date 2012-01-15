6fe1147d60ec7aab28ee905bb306077f24442599
Glenn Washburn <development@efficientek.com>
2012-01-15 01:42:47 -0600

    Ignore NotRegistered exception when unregistering admin sites.

--- userena/admin.py
+++ userena/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.admin.sites import NotRegistered
 from django.utils.importlib import import_module
 from userena.utils import get_profile_model
 from . import settings
@@ -14,11 +15,17 @@ def get_class(path):
 
 UserenaUserAdmin = get_class(settings.USERENA_USERADMIN)
 if UserenaUserAdmin:
-    admin.site.unregister(User)
+    try:
+        admin.site.unregister(User)
+    except NotRegistered:
+        pass
     admin.site.register(User, UserenaUserAdmin)
 
 ProfileAdmin = get_class(settings.USERENA_PROFILEADMIN)
 if ProfileAdmin:
     profile_model = get_profile_model()
-    admin.site.unregister(profile_model)
+    try:
+        admin.site.unregister(profile_model)
+    except NotRegistered:
+        pass
     admin.site.register(profile_model, ProfileAdmin)
